0ac81bb3ba66e9d21137154adb73bd50acc99dee
BhaskarPradhan <135045424+BhaskarPradhan@users.noreply.github.com>
2023-10-17 10:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	function hasKey(key) {
+  // Check if the key exists in the global object sampleObject
+  return sampleObject.hasOwnProperty(key);
+}
+
+// Global object definition
+
+
+// Example usage:
+console.log(hasKey("red"));   // Should return true
+console.log(hasKey("blue"));  // Should return false
+console.log(hasKey("white")); // Should return true
+
 }
 
 // Do not change the code below
